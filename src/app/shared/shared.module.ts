eb0fddb73b9c92889c8c365450b1a52ed9d85adc
naixatwork <naixatwork@gmail.com>
2022-11-20 11:20:56 +0330

    feat(dashboard): introduced mainbar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainBarComponent } from './main-bar/main-bar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainBarComponent
+  ],
   imports: [
     CommonModule
   ]
