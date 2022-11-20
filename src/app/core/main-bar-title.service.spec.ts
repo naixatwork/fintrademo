f71f13b0c8c6e6f8a077c16f6ce8096541cf4447
naixatwork <naixatwork@gmail.com>
2022-11-20 18:59:59 +0330

    feat(dashboard): completed mainbar component and introduced main-bar-title.service.ts

--- src/app/core/main-bar-title.service.spec.ts
+++ src/app/core/main-bar-title.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MainBarTitleService } from './main-bar-title.service';
+import {BehaviorSubject} from "rxjs";
 
 describe('MainBarTitleService', () => {
   let service: MainBarTitleService;
@@ -13,4 +14,11 @@ describe('MainBarTitleService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("should have a #mainBarTitle subject", () => {
+    const title$ = service.mainBarTitle;
+
+    expect(title$).toBeTruthy();
+    expect(title$).toBeInstanceOf(BehaviorSubject);
+  })
 });
