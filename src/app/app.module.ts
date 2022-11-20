367b966456db7786b4e332089e5015c694bd7f35
naixatwork <naixatwork@gmail.com>
2022-11-20 10:49:56 +0330

    feat(primeng): installed primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccordionModule} from "primeng/accordion";
+import {AutoCompleteModule} from "primeng/autocomplete";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
